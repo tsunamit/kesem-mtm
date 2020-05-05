0336f3b2a7b2babe09446126b8ed0327422587ab
Taylor Lee <Taylorlee44@gmail.com>
2020-05-05 16:03:45 -0700

    About Page styling and Footer Component

---
+++ src/components/containers/FooterContainer.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './styles/FooterContainerStyles.css'
+
+function FooterContainer () {
+    return (
+        <div id = 'footer-container'> 
+
+        </div> 
+
+    ); 
+}
+
+export default FooterContainer
