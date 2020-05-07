ee25dc0c8b96d5282263faedc94328eeb8855ae7
Taylor Lee <Taylorlee44@gmail.com>
2020-05-06 23:51:10 -0700

    Progress Bar Style Updates

--- src/components/containers/DonationProgressBar.jsx
+++ src/components/containers/DonationProgressBar.jsx
@@ -4,15 +4,23 @@ import './styles/DonationProgressBarStyles.css'
 
 function DonationProgressBar({ donationGoal, currentDonationTotal }) {
   const percentComplete = Math.min(1.0, currentDonationTotal / donationGoal) * 100;
-
   return (
-    <div>
-      <div className="progress-bar-text"> 
-      {`$0 --- ${currentDonationTotal} --- $${donationGoal}`}
+    <div className="progress-bar-wrapper">
+      <div className="progress-bar-text-wrapper"> 
+        <div className="progress-bar-text">
+          {`$0`}
+        </div>
+        <div className="progress-bar-text">
+          ${currentDonationTotal}
+        </div>
+        <div className="progress-bar-text">
+          ${donationGoal}
+        </div>
       </div> 
-      <div className="progress-bar-graphic"> 
+      <div className="progress-bar-graphic-outline"> 
+        <div className="progress-bar-graphic-fill" style={{width: `${percentComplete}%`}}>
+        </div>
       </div> 
-
     </div>
   );
 }
